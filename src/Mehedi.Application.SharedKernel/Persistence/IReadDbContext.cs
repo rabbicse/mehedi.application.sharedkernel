4c7045f944a7af18903549f54b8a96f29fef4918
Md Mehedi Hasan <rabbi.se@gmail.com>
2024-08-22 17:06:03 +0600

    Update IReadDbContext

--- src/Mehedi.Application.SharedKernel/Persistence/IReadDbContext.cs
+++ src/Mehedi.Application.SharedKernel/Persistence/IReadDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Mehedi.Core.SharedKernel;
+using System.Linq.Expressions;
 
 namespace Mehedi.Application.SharedKernel.Persistence;
 
@@ -9,13 +10,23 @@ namespace Mehedi.Application.SharedKernel.Persistence;
 public interface IReadDbContext
 {
     /// <summary>
+    /// Gets the collection for the specified query model.
+    /// </summary>
+    /// <typeparam name="TQueryModel"></typeparam>
+    /// <returns></returns>
+    Task<IEnumerable<TQueryModel>> GetAllCollectionAsync<TQueryModel>() 
+        where TQueryModel : IQueryModel;
+
+    /// <summary>
     /// Gets the collection for the specified query model with page.
     /// </summary>
     /// <typeparam name="TQueryModel"></typeparam>
     /// <param name="start"></param>
     /// <param name="pageSize"></param>
     /// <returns></returns>
-    Task<IEnumerable<TQueryModel>> GetCollectionAsync<TQueryModel>(int start = 0, int pageSize = 100) where TQueryModel : IQueryModel;
+    Task<IEnumerable<TQueryModel>> GetCollectionAsync<TQueryModel>(int start = 0, int pageSize = 100) 
+        where TQueryModel : IQueryModel;
+
     /// <summary>
     /// Gets the Query Model by id.
     /// </summary>
@@ -24,6 +35,34 @@ public interface IReadDbContext
     /// <param name="id"></param>
     /// <returns></returns>
     Task<TQueryModel?> GetByIdAsync<TQueryModel, TKey>(TKey id) where TQueryModel : IQueryModel;
+
+    /// <summary>
+    /// Upserts a query model into the database.
+    /// </summary>
+    /// <typeparam name="TQueryModel">The type of the query model.</typeparam>
+    /// <param name="queryModel">The query model to upsert.</param>
+    /// <param name="upsertFilter">The filter expression to determine the upsert condition.</param>
+    /// <returns>A task representing the asynchronous upsert operation.</returns>
+    Task UpsertAsync<TQueryModel>(TQueryModel queryModel, Expression<Func<TQueryModel, bool>> upsertFilter)
+        where TQueryModel : IQueryModel;
+
+    /// <summary>
+    /// Upserts query models into the database.
+    /// </summary>
+    /// <typeparam name="TQueryModel"></typeparam>
+    /// <param name="queryModels"></param>
+    /// <returns></returns>
+    Task UpsertAsync<TQueryModel>(IEnumerable<TQueryModel> queryModels)
+    where TQueryModel : IQueryModel;
+
+    /// <summary>
+    /// Deletes query models from the database that match the specified filter.
+    /// </summary>
+    /// <typeparam name="TQueryModel">The type of the query model.</typeparam>
+    /// <param name="deleteFilter">The filter expression to determine which query models to delete.</param>
+    /// <returns>A task representing the asynchronous delete operation.</returns>
+    Task DeleteAsync<TQueryModel>(Expression<Func<TQueryModel, bool>> deleteFilter)
+        where TQueryModel : IQueryModel;
 
     /// <summary>
     /// Get read database context
